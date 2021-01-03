c42d8d7e3540ab9dc481a4b8d679f74dbf5998a3
bivek520 <57966410+bivek520@users.noreply.github.com>
2021-01-03 12:40:50 -0600

    added font awesome package and updated landing page

--- src/component/landingPage.jsx
+++ src/component/landingPage.jsx
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class LandingPage extends Component {
 	render() {
 		return (
-			<div>
-				<h1>Landing Page</h1>
+			<div style={{ width: '100%', margin: 'auto' }}>
+				<Grid className='landing-grid'>
+					<Cell col={12}>
+						<img
+							src='https://avatars0.githubusercontent.com/u/57966410?s=460&u=d30c6289baa567f5ca27d55afbd8683bed955a2a&v=4'
+							alt='Profile_pic'
+							className='profile-img'
+						/>
+					</Cell>
+					<div className='banner-text'>
+						<h1>Bivek Shrestha</h1>
+
+						<hr />
+
+						<p>C | C++ | Python | SQL |JavaScript | React | Laravel | MongoDB</p>
+					</div>
+				</Grid>
 			</div>
 		);
 	}
